1ddb0523dc5e7f2a99be787b2c6bc6d7f880068f
Vishwanath Krishnappanavar <Vishwanath.Krishnappanavar@infostretch.com>
2020-02-07 12:51:47 +0530

    About page change

--- src/containers/About.js
+++ src/containers/About.js
@@ -10,7 +10,7 @@ const About = () => {
         <li>Redux-saga : <span>Redux middleware library used to handle side effects.</span></li>
         <li>Testing : <span>Have used <b>JEST, Enzyme, react-test-renderer, and redux-mock-store</b> for testing.JEST to run tests , Enzyme for making assertions, react-test-renderer to create snapshots of the component and redux-mock-store to create fake redux store for testing.Tests are in place for components with both snapshot and functionality testing.Tests are also in place for Actions, Reducers, and Sagas.</span></li>
         <li>Github link : <a href="https://github.com/vishwa3/videos-photos-library-2" target="_blank">https://github.com/vishwa3/videos-photos-library</a></li>
-        <li>Application deployed on <b>NETLIFY</b></li>
+        <li>Application deployed on <b>NETLIFY</b> <a href="https://www.netlify.com/" target="_blank">https://www.netlify.com/</a></li>
       </ul>
     </div>
   )
